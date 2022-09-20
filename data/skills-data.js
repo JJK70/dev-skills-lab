13acba24f0594a53e614359096ca2940e169605f
JJK70 <jjk701@gmail.com>
2022-09-20 19:49:04 -0400

    set up skills index.ejs file

--- data/skills-data.js
+++ data/skills-data.js
@@ -1,9 +1,10 @@
 const skills = [
   {text: 'HTML', skilled: true, _id: 125223},
-  {text: 'CSS', skilled: false, _id: 127904},
-  {text: 'Javascript', skilled: false, _id: 139608},
-  {text: 'Node', skilled: false, _id: 139608},
-  {text: 'EJS', skilled: false, _id: 139608},
+  {text: 'CSS', skilled: true, _id: 127904},
+  {text: 'Javascript', skilled: true, _id: 139608},
+  {text: 'Node', skilled: true, _id: 139608},
+  {text: 'EJS', skilled: true, _id: 139608},
+  {text: 'MongoDB', skilled: false, _id: 139608},
 ]
 
 export { 
