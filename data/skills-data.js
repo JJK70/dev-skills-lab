558d8df44f5a4e823057a09fdf3f50f69f9fdb50
JJK70 <jjk701@gmail.com>
2022-09-20 19:38:29 -0400

    add data skills

---
+++ data/skills-data.js
@@ -0,0 +1,11 @@
+const skills = [
+  {text: 'HTML', skilled: true, _id: 125223},
+  {text: 'CSS', skilled: false, _id: 127904},
+  {text: 'Javascript', skilled: false, _id: 139608},
+  {text: 'Node', skilled: false, _id: 139608},
+  {text: 'EJS', skilled: false, _id: 139608},
+]
+
+export { 
+	skills
+}
